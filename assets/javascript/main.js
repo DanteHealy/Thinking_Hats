700c084cf59e5f89eddd02d6f66fdfbf9907975e
DanteHealy <dantephealy@gmail.com>
2020-08-04 08:05:21 +0000

    Get the modal working. Add some JavaScript to enable the main question to appear on the header and clean up some of the style elements on the index.html page

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,14 @@
-$('#white-modal').on('shown.bs.modal', function () {
-  $('#white-modal').trigger('focus');
+// Create two variables for element ids
+
+let questionInput = document.getElementById('question-problem');
+let submitButton = document.getElementById('submit');
+let questionDisplay = document.getElementById('question-display');
+let clearButton = document.getElementById('clear');
+
+submitButton.addEventListener("click",function(){
+    questionDisplay.textContent = questionInput.value;
+});
+
+clearButton.addEventListener("click",function(){
+    questionDisplay.textContent = "Enter Your Question or Challenge to Display Here!";
 });
